cd99f5604f85c72adaed41b6365d2a0c75fb15f4
Josh <mrjoshuasperry@gmail.com>
2018-02-07 17:46:55 -0500

    Add initial files

---
+++ src/main/java/Rifts.java
@@ -0,0 +1,13 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Rifts extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
