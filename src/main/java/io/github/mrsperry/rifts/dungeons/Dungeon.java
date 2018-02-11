dda6172807f0e17af16af9296b88697f94a514c3
Josh <mrjoshuasperry@gmail.com>
2018-02-11 06:04:51 -0500

    Start dungeon work

--- src/main/java/io/github/mrsperry/rifts/dungeons/Dungeon.java
+++ src/main/java/io/github/mrsperry/rifts/dungeons/Dungeon.java
@@ -1,13 +1,43 @@
 package io.github.mrsperry.rifts.dungeons;
 
-public class Dungeon {
+import org.bukkit.entity.Monster;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+
+import java.util.HashSet;
+
+public abstract class Dungeon implements Listener {
     private DungeonConfig config;
 
-    public Dungeon(DungeonConfig config) {
+    private HashSet<Monster> monsters;
+    private Monster boss;
+
+    public Dungeon(DungeonConfig config, Player keyholder) {
         this.config = config;
+
+        this.monsters = new HashSet<Monster>();
+        this.boss = null;
     }
 
-    public void teleport() {
+    public void teleport(Player keyholder, String message) {
+        HashSet<Player> nearby = new HashSet<Player>();
+        nearby.add(keyholder);
+        for (Player player : keyholder.getWorld().getPlayers()) {
+            if (keyholder.getLocation().distance(player.getLocation()) <= 5) {
+                nearby.add(player);
+            }
+        }
+
+        for (Player player : nearby) {
+            player.teleport(config.getStartingLocation());
+            player.sendMessage(message);
+        }
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
 
     }
 }
