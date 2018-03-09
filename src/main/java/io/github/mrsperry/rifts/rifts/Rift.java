cf60f1542cf9a7ecdeb586b0fd29a12f009ac547
Josh <mrjoshuasperry@gmail.com>
2018-03-09 11:47:21 -0500

    Stop removing double the monsters

--- src/main/java/io/github/mrsperry/rifts/rifts/Rift.java
+++ src/main/java/io/github/mrsperry/rifts/rifts/Rift.java
@@ -113,7 +113,6 @@ public class Rift implements Runnable, Listener {
                     LivingEntity entity = monsters.get(0);
                     if (!entity.isDead()) {
                         entity.damage(entity.getHealth());
-                        monsters.remove(0);
                         entity.getWorld().spawnParticle(Particle.PORTAL, entity.getLocation().add(0, 1, 0), 5);
                     } else {
                         monsters.remove(0);
