317fe729e6c2c33ac1ee1ea8dd78199934c2bd10
Josh <mrjoshuasperry@gmail.com>
2018-03-12 16:08:46 -0400

    Remove check for particles

--- src/main/java/io/github/mrsperry/rifts/Timer.java
+++ src/main/java/io/github/mrsperry/rifts/Timer.java
@@ -3,12 +3,10 @@ package io.github.mrsperry.rifts;
 import io.github.mrsperry.rifts.configs.GeneralConfig;
 import io.github.mrsperry.rifts.configs.RiftConfig;
 import io.github.mrsperry.rifts.meta.Rift;
-import io.github.mrsperry.rifts.meta.RiftEffect;
 import io.github.mrsperry.rifts.utils.SpawnUtils;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 
 import java.util.Collection;
@@ -25,16 +23,6 @@ public class Timer implements Runnable {
     public static boolean spawnRift(Player player, RiftConfig config) {
         Location location = SpawnUtils.getValidLocation(player.getLocation(), GeneralConfig.getMinArea(), GeneralConfig.getMaxArea());
         if (location != null) {
-            for (Location core : RiftEffect.getCoreLocations(location)) {
-                if (core.getBlock().getType() != Material.AIR) {
-                    return false;
-                }
-            }
-            for (Location secondary : RiftEffect.getSecondaryLocations(location)) {
-                if (secondary.getBlock().getType() != Material.AIR) {
-                    return false;
-                }
-            }
             Bukkit.broadcastMessage("Location: " + location + "  Distance: " + player.getLocation().distance(location));
             new Rift(location, config);
         }
