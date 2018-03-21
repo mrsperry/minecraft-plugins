1e1cbc469021e45b7f08f6c5011f004624c8a0c6
Josh <mrjoshuasperry@gmail.com>
2018-03-21 04:14:26 -0400

    Add compressed mobs

---
+++ compressed-mobs/README.md
@@ -0,0 +1,2 @@
+# Compressed Mobs
+Allows you to "compress" animals and monsters so that when they die more entities of the same type will spawn.
---
+++ compressed-mobs/src/main/java/io/github/mrsperry/compressedmobs/Main.java
@@ -0,0 +1,81 @@
+package io.github.mrsperry.compressedmobs;
+
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+import java.util.HashSet;
+import java.util.Random;
+
+public class Main extends JavaPlugin implements Listener {
+    private Random random;
+
+    private int chance;
+    private int yield;
+    private HashSet<EntityType> blacklist;
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+
+        this.random = new Random();
+
+        FileConfiguration config = this.getConfig();
+        this.chance = config.getInt("chance-to-compress", 15);
+        this.yield = config.getInt("yield-when-killed", 5);
+        this.blacklist = new HashSet<EntityType>();
+        if (config.isList("blacklist")) {
+            for (String mob : config.getStringList("blacklist")) {
+                try {
+                    this.blacklist.add(EntityType.valueOf(mob.toUpperCase()));
+                } catch (Exception ex) {
+                    this.getLogger().severe("Invalid entity type: " + mob);
+                }
+            }
+        }
+        this.getLogger().info("Found " + this.blacklist.size() + " types on the blacklist");
+
+        this.getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+        if (!this.blacklist.contains(event.getEntityType())) {
+            Entity mob = event.getEntity();
+            if (mob.getCustomName() != null) {
+                if (mob.getCustomName().equals(ChatColor.GRAY + "Compressed " + mob.getName().replace(ChatColor.GRAY + "Compressed ", ""))) {
+                    for (int amount = 0; amount < this.yield; amount++) {
+                        Entity entity = mob.getWorld().spawnEntity(mob.getLocation(), mob.getType());
+                        entity.setVelocity(new Vector(
+                                (this.random.nextDouble() * 2) - 1,
+                                (this.random.nextDouble() / 2),
+                                (this.random.nextDouble() * 2) - 1));
+                    }
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onCreatureSpawn(CreatureSpawnEvent event) {
+        if (!this.blacklist.contains(event.getEntityType())) {
+            Entity creature = event.getEntity();
+            if (this.random.nextInt(100) <= this.chance) {
+                // Use names instead of UUIDs; Entities aren't assigned IDs until after the spawn event
+                creature.setCustomName(ChatColor.GRAY + "Compressed " + creature.getName());
+                creature.setCustomNameVisible(true);
+            }
+
+            if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SLIME_SPLIT) {
+                event.getEntity().setCustomName("");
+            }
+        }
+    }
+}
