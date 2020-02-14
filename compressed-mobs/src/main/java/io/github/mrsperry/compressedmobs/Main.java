5659a8afedd8352be330c0e55a4e5eee8701ae41
Josh <mrjoshuasperry@gmail.com>
2020-02-14 15:16:37 -0500

    Change compression system from names to persistant NBT data

--- compressed-mobs/src/main/java/io/github/mrsperry/compressedmobs/Main.java
+++ compressed-mobs/src/main/java/io/github/mrsperry/compressedmobs/Main.java
@@ -2,13 +2,15 @@ package io.github.mrsperry.compressedmobs;
 
 import net.md_5.bungee.api.ChatColor;
 
-import org.bukkit.Bukkit;
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
@@ -54,17 +56,17 @@ public class Main extends JavaPlugin implements Listener {
     public void onEntityDeath(EntityDeathEvent event) {
         if (!this.blacklist.contains(event.getEntityType())) {
             Entity mob = event.getEntity();
-            if (mob.getCustomName() != null) {
-                if (mob.getCustomName().equals(ChatColor.GRAY + "Compressed " + mob.getName().replace(ChatColor.GRAY + "Compressed ", ""))) {
-                    int range = this.maxYield - this.minYield;
-                    int total = this.minYield + (range > 0 ? this.random.nextInt(range) : 0);
-                    for (int amount = 0; amount < total; amount++) {
-                        Entity entity = mob.getWorld().spawnEntity(mob.getLocation(), mob.getType());
-                        entity.setVelocity(new Vector(
-                                (this.random.nextDouble() * 2) - 1,
-                                (this.random.nextDouble() / 2),
-                                (this.random.nextDouble() * 2) - 1));
-                    }
+            PersistentDataContainer container = mob.getPersistentDataContainer();
+
+            if (container.has(new NamespacedKey(this, "compressed"), PersistentDataType.BYTE)) {
+                int range = this.maxYield - this.minYield;
+                int total = this.minYield + (range > 0 ? this.random.nextInt(range) : 0);
+                for (int amount = 0; amount < total; amount++) {
+                    Entity entity = mob.getWorld().spawnEntity(mob.getLocation(), mob.getType());
+                    entity.setVelocity(new Vector(
+                            (this.random.nextDouble() * 2) - 1,
+                            (this.random.nextDouble() / 2),
+                            (this.random.nextDouble() * 2) - 1));
                 }
             }
         }
@@ -74,14 +76,17 @@ public class Main extends JavaPlugin implements Listener {
     public void onCreatureSpawn(CreatureSpawnEvent event) {
         if (!this.blacklist.contains(event.getEntityType())) {
             Entity creature = event.getEntity();
-            if (this.random.nextDouble() * this.chance <= 1) {
-                // Use names instead of UUIDs; Entities aren't assigned IDs until after the spawn event
-                creature.setCustomName(ChatColor.GRAY + "Compressed " + creature.getName());
-                creature.setCustomNameVisible(true);
+            PersistentDataContainer container = creature.getPersistentDataContainer();
+
+            if (container.has(new NamespacedKey(this, "compressed"), PersistentDataType.BYTE)) {
+                return;
             }
 
-            if (event.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SLIME_SPLIT) {
-                event.getEntity().setCustomName("");
+            if (this.random.nextDouble() * this.chance <= 1) {
+                container.set(new NamespacedKey(this, "compressed"), PersistentDataType.BYTE, (byte) 1);
+
+                creature.setCustomName(ChatColor.GRAY + "Compressed " + creature.getName());
+                creature.setCustomNameVisible(true);
             }
         }
     }
