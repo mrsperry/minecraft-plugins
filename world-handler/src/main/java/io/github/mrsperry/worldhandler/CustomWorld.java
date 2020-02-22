5c6aec66c2f68a5f2ca1686f471efdb1aa7306a6
Josh <mrjoshuasperry@gmail.com>
2020-02-22 07:20:30 -0500

    Add a settings display

--- world-handler/src/main/java/io/github/mrsperry/worldhandler/CustomWorld.java
+++ world-handler/src/main/java/io/github/mrsperry/worldhandler/CustomWorld.java
@@ -1,5 +1,7 @@
 package io.github.mrsperry.worldhandler;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.Difficulty;
 import org.bukkit.GameMode;
 import org.bukkit.World;
@@ -8,17 +10,17 @@ public class CustomWorld {
     /** The normal world object */
     private World world;
     /** The default game mode */
-    private GameMode gameMode;
+    private GameMode gameMode = GameMode.SURVIVAL;
     /** The default difficulty */
-    private Difficulty difficulty;
+    private Difficulty difficulty = Difficulty.HARD;
     /** If animals can spawn */
-    private boolean animals;
+    private boolean animals = true;
     /** If monsters can spawn */
-    private boolean monsters;
+    private boolean monsters = true;
     /** If weather events can occur */
-    private boolean weather;
+    private boolean weather = true;
     /** If the time of day should be locked */
-    private boolean timeLock;
+    private boolean timeLock = true;
 
     /**
      * Creates a new custom world
@@ -26,6 +28,16 @@ public class CustomWorld {
      */
     protected CustomWorld(World world) {
         this.world = world;
+    }
+
+    public String getSettings() {
+        return ChatColor.DARK_GRAY + "========== " + ChatColor.AQUA + this.world.getName() + ChatColor.DARK_GRAY + " ==========\n"
+            + ChatColor.GRAY + "Game Mode: " + ChatColor.GREEN + this.gameMode.toString() + "\n"
+            + ChatColor.GRAY + "Difficulty: " + ChatColor.GREEN + this.difficulty.toString() + "\n"
+            + ChatColor.GRAY + "Animals: " + (this.animals ? ChatColor.GREEN : ChatColor.RED) + this.animals + "\n"
+            + ChatColor.GRAY + "Monsters: " + (this.monsters ? ChatColor.GREEN : ChatColor.RED) + this.monsters + "\n"
+            + ChatColor.GRAY + "Weather: " + (this.weather ? ChatColor.GREEN : ChatColor.RED) + this.weather + "\n"
+            + ChatColor.GRAY + "Time Lock: " + (this.timeLock ? ChatColor.GREEN : ChatColor.RED) + this.timeLock;
     }
 
     public World getWorld() {
