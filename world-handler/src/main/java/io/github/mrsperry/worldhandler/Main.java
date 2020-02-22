654b0929d184e374c9b8d942d4f2e456d6d45640
Josh <mrjoshuasperry@gmail.com>
2020-02-22 03:14:17 -0500

    Make the main class a singleton and create a random

--- world-handler/src/main/java/io/github/mrsperry/worldhandler/Main.java
+++ world-handler/src/main/java/io/github/mrsperry/worldhandler/Main.java
@@ -2,14 +2,28 @@ package io.github.mrsperry.worldhandler;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Random;
+
 public class Main extends JavaPlugin {
+    private static Main instance;
+
+    private final Random random = new Random();
+
     @Override
     public void onEnable() {
-
+        Main.instance = this;
     }
 
     @Override
     public void onDisable() {
         
+    }
+
+    public static Main getInstance() {
+        return Main.instance;
+    }
+
+    public Random getRandom() {
+        return this.random;
     }
 }
